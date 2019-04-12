6f2146eedd2441a5e1522ea93b9a0cf8d0a307fe
Anwer Ullah <Anwer Ullah@DESKTOP-EB9HOQL>
2019-04-12 10:23:11 +0600

    startup chage

--- UdemyCoreCourse/Startup.cs
+++ UdemyCoreCourse/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 
 namespace UdemyCoreCourse
 {
