218f9a44c56494827d394483b17bf01b6ce0ec02
Anwer Ullah <Anwer Ullah@DESKTOP-EB9HOQL>
2019-04-12 09:34:45 +0600

    First Commit

---
+++ UdemyCoreCourse/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace UdemyCoreCourse.Models
+{
+    public class Category
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
